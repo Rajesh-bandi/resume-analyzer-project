fd18f36f9f718eb7774b557d2888141edb952fbe
BANDI RAJESH <bandirajesh209@gmail.com>
2026-03-29 17:23:52 +0530

    working updated code

--- app.py
+++ app.py
@@ -90,6 +90,7 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+
 if __name__ == "__main__":
-    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
     app.run(debug=True, use_reloader=False)
